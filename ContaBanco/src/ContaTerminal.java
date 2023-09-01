288359ee732de33982401282623e4f0f40dbf925
Bruno Henrique Marinho Marques <brunohenriw1@gmail.com>
2023-09-01 14:54:36 -0300

    leitura dos dados via prompt e impressão

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Criando uma instancia do tipo Scanner e criando as variaveis
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+
+        //exibindo mensagem e recebendo o que for digitado pelo usuario
+        System.out.println("Por favor, digite o número da Agência !");
+        numero = scanner.nextInt();
+        
+        //exibindo mensagem e recebendo o que for digitado pelo usuario
+        System.out.println("Agora digite a Agência por favor: ");
+        agencia = scanner.next();
+
+        //exibindo mensagem e recebendo o que for digitado pelo usuario
+        System.out.println("Por favor, digite seu nome: ");
+        nomeCliente = scanner.next();
+
+        //limpando buffer do teclado
+        scanner.nextLine();
+
+        //exibindo mensagem e recebendo o que for digitado pelo usuario
+        System.out.println("Digite seu saldo: ");
+        saldo = scanner.nextDouble();
+
+        //exibindo mensagem com as informações do usuario
+        System.out.println("Olá "+nomeCliente+ ", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+" conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
+
+
+
+
+
     }
 }
